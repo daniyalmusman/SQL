977a73a9111bedfeb683121ff809983c47453d73
Daniyal Usman <daniyal.m.usman@gmail.com>
2026-06-08 14:37:48 +0500

    first commit

---
+++ Advanced_Queries/Query-3.sql
@@ -0,0 +1,19 @@
+
+
+SELECT 
+    skills_dim.skill_id,
+    skills_dim.skills,
+    count(job_postings_fact.job_id) AS skill_demand
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim
+ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim
+ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Data Analyst'
+GROUP BY
+    skills_dim.skills, skills_dim.skill_id
+ORDER BY 
+    skill_demand DESC
+LIMIT 5;
